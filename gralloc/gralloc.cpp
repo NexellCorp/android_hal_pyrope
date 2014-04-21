92d5b250bad76bb1b250ccf09edaee0069d0a42e
Sungwoo Park <swpark@nexell.co.kr>
2014-04-21 14:23:35 +0900

    remove 16 align of cb,cr stride

--- gralloc/gralloc.cpp
+++ gralloc/gralloc.cpp
@@ -162,8 +162,8 @@ static int gralloc_alloc_framework_yuv(int ionfd, int w, int h, int format, int 
 
     switch (format) {
     case HAL_PIXEL_FORMAT_YV12:
-        //size = (stride * vstride) + (ALIGN(stride >> 1, 16) * vstride);
-        size = (stride * vstride) + (ALIGN(stride >> 1, 16) * ALIGN(h >> 1, 16)) * 2;
+        //size = (stride * vstride) + (ALIGN(stride >> 1, 16) * ALIGN(h >> 1, 16)) * 2;
+        size = (stride * vstride) + (((stride >> 1) * (h >> 1)) * 2);
         break;
     case HAL_PIXEL_FORMAT_YCrCb_420_SP:
         //size = (stride * vstride * 3) >> 1; // y size * 1.5
