120f457d2201672972ebc1e5b19a4f86e18a7172
Kim seonghee <kshblue@nexell.co.kr>
2014-09-25 19:44:15 +0900

    1) modified because of updated video api

--- lib_jpeghw/NXJpegHWEnc.cpp
+++ lib_jpeghw/NXJpegHWEnc.cpp
@@ -49,7 +49,7 @@ int NX_JpegHWEncoding(void *dstVirt, int dstSize,
     encInitParam.mirDirection = 0;
     encInitParam.jpgQuality = 100;
 
-    hEnc = NX_VidEncOpen(NX_JPEG_ENC);
+    hEnc = NX_VidEncOpen(NX_JPEG_ENC, NULL);
     if (NX_VidEncInit(hEnc, &encInitParam) != 0) {
         ALOGE("NX_VidEncInit failed!!!");
         return -EIO;
